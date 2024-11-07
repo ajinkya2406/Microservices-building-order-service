eb2b0a837de96cacbdd32a3cde43214d46653d76
ajju06 <pewdikai.gamer@gmail.com>
2024-11-07 18:27:15 +0530

    Order-Service build

---
+++ order-service/src/main/java/com/ajinkyaprogrm/order_service/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.ajinkyaprogrm.order_service.controller;
+
+
+import com.ajinkyaprogrm.order_service.dto.OrderRequest;
+import com.ajinkyaprogrm.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "order placed successfully";
+    }
+
+
+}
---
+++ order-service/src/main/java/com/ajinkyaprogrm/order_service/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ajinkyaprogrm.order_service.repository;
+
+import com.ajinkyaprogrm.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+
+}
